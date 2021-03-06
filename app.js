cc83b57c8f6a3c1bb6ac44a4e79b754e22f17175
calmighted <calmighted@gmail.com>
2021-03-06 12:33:16 +0530

    initial commit

---
+++ app.js
@@ -0,0 +1,30 @@
+import API_KEY from "./apikey.js";
+
+
+
+let locationInput = document.querySelector('.location-input');
+let searchBtn = document.querySelector('.search-btn');
+
+
+let cityName = 'london';
+
+
+
+const sendLocation = () => {
+    console.log(locationInput.value);
+    cityName = locationInput.value;
+    fetch(`http://api.openweathermap.org/data/2.5/weather?q=${cityName}&appid=${API_KEY}`, {mode: 'cors'})
+    .then(function(res){
+        return res.json();
+    })
+    .then(function(res){
+        console.log(res);
+        console.log(res.main.temp)
+    })
+    .catch(err => console.log(err))
+    locationInput.value = '';
+}
+
+
+
+searchBtn.addEventListener('click',sendLocation);
